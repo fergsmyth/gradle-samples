fed7b0a8bf8c126a7539d8d87efa0442e7ca0784
FergSmyth <fergsmyth@gmail.com>
2014-03-23 13:10:35 +0000

    Changed .gitingnore added Sample files

---
+++ integration-test/src/main/java/com/genericgames/Sample.java
@@ -0,0 +1,13 @@
+package com.genericgames;
+
+public class Sample {
+	
+	public boolean isTrue(){
+		return true;
+	}
+	
+	public int numberOne(){
+		return 1;
+	}
+}
+
---
+++ integration-test/src/main/java/com/genericgames/SampleInt.java
@@ -0,0 +1,13 @@
+package com.genericgames;
+
+public class SampleInt {
+	
+	public boolean isTrue(){
+		return true;
+	}
+	
+	public int numberOne(){
+		return 1;
+	}
+}
+
